98e8ca62902f4fa6464697b084b1a08be3004153
aashish <ritikkamboj4314@gmail.com>
2026-02-04 19:25:07 +0530

    issue sorted when we hitting the /sendPayload api

--- src/workers/evaluationWorker.js
+++ src/workers/evaluationWorker.js
@@ -7,7 +7,7 @@ function evaluationWorker(queue) {
   new Worker(queue, async (job) => {
     if (job.name === "EvaluationJob") {
   try {
-      const response =  axios.post("http://localhost:3002/sendPayload", {
+      const response =  await axios.post("http://localhost:3002/sendPayload", {
         userId : job.data.userId,
         payload : job.data
       })
